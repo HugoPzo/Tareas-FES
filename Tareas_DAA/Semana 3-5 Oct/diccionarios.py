5b42118bf69954146d9e138025c4775819edde0a
hugo.pzz <hugo.peror.04@gmail.com>
2024-10-03 11:19:20 -0600

    Clase 3-5 Oct

---
+++ Tareas_DAA/Semana 3-5 Oct/diccionarios.py
@@ -0,0 +1,49 @@
+import string
+import matplotlib.pyplot as plt
+
+
+def cifra_sustitucion(cad, llave):
+    cifrado = ""
+    for c in cad:
+        if c in alphabet:
+            pos = alphabet.index(c)
+            cifrado += llave[pos]
+        else:
+            cifrado += c
+    return cifrado
+
+
+alphabet = string.ascii_lowercase
+alpha_dic = {}
+llave = "mansbdvfcgxhzjkploqwietyru"
+
+
+
+
+print(alphabet)
+
+num = 0
+for letter in alphabet:
+    alpha_dic[letter] = num
+    num += 1
+
+cad = "Esta es una prueba de texto para probar algunas estructuras de datos con unos alumnos que ya no se han de acordar de lo ultimo que vieron porque ya tienen un monton de tiempo sin clases"
+
+texto = cifra_sustitucion(cad, llave)
+print(texto)
+
+for letter in texto:
+    if letter in alphabet:
+        alpha_dic[letter] += 1
+    
+
+datos = []
+for d in alpha_dic.values():
+    datos.append(d)
+
+plt.plot(alpha_dic.values())
+plt.show()
+
+print(alpha_dic)
+
+
---
+++ Tareas_DAA/Semana 3-5 Oct/xd.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+
+
+label = Label(window,
+            text="URI ES ",
+            font=("Arial", 84))
+
+label.pack()
+window.mainloop()
